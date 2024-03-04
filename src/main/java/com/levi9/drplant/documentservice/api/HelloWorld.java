6b64d5976bbed90670533149ce17a940938b6183
Aleksandar Ivanovic <aleksandarivanovicmaster@gmail.com>
2024-03-04 14:35:26 +0100

    Latest connectio to RDS instead mysql on ec2 instance.

--- src/main/java/com/levi9/drplant/documentservice/api/HelloWorld.java
+++ src/main/java/com/levi9/drplant/documentservice/api/HelloWorld.java
@@ -1,14 +1,26 @@
 package com.levi9.drplant.documentservice.api;
 
+import com.levi9.drplant.documentservice.db.entity.ImageMetadata;
+import com.levi9.drplant.documentservice.db.repository.ImageMetadataRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class HelloWorld {
+    @Autowired
+    ImageMetadataRepository imageMetadataRepository;
 
     @GetMapping("/hello")
     public ResponseEntity<String> helloWorld() {
         return ResponseEntity.ok().body("Hello world");
+    }
+
+    @GetMapping("/hello-rds")
+    public ResponseEntity<List<ImageMetadata>> helloRDS() {
+        return ResponseEntity.ok(imageMetadataRepository.findAll());
     }
 }
